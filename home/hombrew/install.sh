1a6ccf5b2165d5ce9cc2088af2a83749f50a8c4c
rafid <rafidibnsadik@gmail.com>
2025-12-09 08:16:09 +0600

    Add Homebrew installation script
    This script installs Homebrew and links keg-only formulas.

---
+++ home/hombrew/install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+  read
+  if test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  fi
+
+fi
+
+# Link keg-only formulas that we want in PATH
+brew list | grep 'postgresql@' | xargs -I {} brew link {} --force 2>/dev/null
+
+exit 0
